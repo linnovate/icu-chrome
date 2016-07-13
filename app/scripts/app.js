5cd73a11cce68d0f6b744202b08e3bb4f19ac275
Avraham Ostreicher <avraham@linnovate.net>
2016-07-13 17:20:25 +0300

    connecting to Google Calendar API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,64 @@
 'use strict';
 
-angular.module("app", []);
+angular.module("app", [])
+    .controller('icuCtrl', function ($scope, $http) {
 
+    $scope.tabs = ['meetings','tasks','project'].map(function(item, i){
+        return {
+            id: i,
+            title: item,
+            items: []
+        }
+    })
 
-angular.module('app').controller('icuCtrl', function ($scope) {
-
-   var tabs = [
-            {
-                'tabId': 1,
-                'title': 'meetings',
-            },
-            {
-                'tabId': 2,
-                'title': 'tasks',
-            },
-            {
-               'tabId': 3,
-                'title': 'project',
+    var getProfileInfo = function(token) {
+        $http({
+            method: 'GET',
+            url: 'https://content.googleapis.com/plus/v1/people/me',
+            headers: {
+                'Authorization': 'Bearer ' + token
             }
-        ]; 
+        }).then(function(res){
+            $scope.profile = {
+                image: res.data.image.url,
+                name: res.data.name.givenName
+            }
+        })
+    }
 
-    $scope.tabs = tabs;
+    var getCalendarEvents = function(token) {
+        $http({
+            method: 'GET',
+            url: 'https://content.googleapis.com/calendar/v3/calendars/primary/events',
+            params: {
+                timeMin: new Date(1465826445308).toISOString(), // 1465826445308
+                singleEvents: true,
+                showDeleted: false,
+                orderBy: 'startTime',
+                maxResults: 50
+            },
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(function(res){
+            console.dir(res.data.items)
+            $scope.tabs[0].items = res.data.items.map(function(item){
+                return item
+            })
+        })
+    }
+
+    chrome.identity.getAuthToken({
+        interactive: true
+    }, function(token){
+        console.log(token)
+        getCalendarEvents(token)
+        getProfileInfo(token)
+        chrome.identity.getProfileUserInfo(function(info){
+            console.log(info)
+        })
+    })
+
     $scope.selected = 0;
 
     $scope.select= function(index) {
