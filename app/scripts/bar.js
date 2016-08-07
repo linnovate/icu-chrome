9bfe6af19684d1d3fa59f5d9d68d52fb21b091f3
Avraham Ostreicher <avraham@linnovate.net>
2016-08-07 15:12:47 +0300

    fixing style

--- app/scripts/bar.js
+++
@@ -1,6 +0,0 @@
-var toggle = document.getElementById('toggle-left-bar');
-var bar = document.getElementById('left-bar');
-
-toggle.onclick = function() {
-  bar.className = bar.className ? '': 'open';
-}
