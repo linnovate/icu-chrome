799719ddb05fe98ba3a2f2e6aa5e15b4eb3bbe53
Avraham Ostreicher <avraham@linnovate.net>
2016-08-07 14:41:49 +0300

    deleted unused files

--- app/scripts/settings.js
+++
@@ -1,7 +0,0 @@
-var settings = {
-  server: 'http://root.hrm.demo.linnovate.net/api/tasks',
-  projectLink: 'http://root.hrm.demo.linnovate.net/tasks/by-project/',
-  authentication: "Bearer eyJhbGciOiJIUzI1NiJ9.JTdCJTIyX2lkJTIyOiUyMjU1YzBiMjU2YjRkNTZmODU0NDJkMTNjOSUyMiwlMjJuYW1lJTIyOiUyMlJhZmFlbCUyMiwlMjJlbWFpbCUyMjolMjJyYWZhZWxAbGlubm92YXRlLm5ldCUyMiwlMjJ1c2VybmFtZSUyMjolMjJyYWZhZWxiJTIyLCUyMl9fdiUyMjowLCUyMnByb3ZpZGVyJTIyOiUyMmxvY2FsJTIyLCUyMnJvbGVzJTIyOiU1QiUyMmF1dGhlbnRpY2F0ZWQlMjIlNUQlN0Q.qtk5vD7m-gFSRUaKxRa-cabMu0ObNSXvrRLuxaFsyqU",
-  project: "57a0ab62129f81a55d63cb4f"
-};
-
